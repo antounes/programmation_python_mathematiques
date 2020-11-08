308bbe1e0c0fdcaf373eed3a4dfb6bdfcb8dceb7
antounes <matthieu.brito.antunes@gmail.com>
2020-11-08 22:28:08 +0100

    2.2 Modules - Quelques modules 'batteries included'

--- 2_modules/montecarlo2.py
+++ 2_modules/montecarlo2.py
@@ -2,20 +2,22 @@
 #-*- coding: Utf-8 -*-
 
 import random
+import sys
 
 from math import sqrt, hypot, pi
 
 def f(x, y, a, b):
     c = sqrt(a**2 - b**2)
-    return hypot(x-c, y) + hypoth(x+c, y)
+    return hypot(x-c, y) + hypot(x+c, y)
 
 def montecarlo2(a, b, n):
     xrandom = [random.uniform(-a, a) for i in range(n)]
     yrandom = [random.uniform(-b, b) for i in range(n)]
 
-    s = sum(f(x, y, a, b)) for x in xrandom
-        for y in yrandom if hypot(x/a, y/b) <= 1
 
+    s = sum(f(x, y, a, b) for x in xrandom
+            for y in yrandom if hypot(x/a, y/b) <= 1) 
+            
     return s * (4 * a * b) / n**2
 
 print('-'*63)
@@ -30,6 +32,6 @@ for i in range(1, 5):
     erreur = val_exacte - approx
     erreur_relative = abs((val_exacte - approx)/val_exacte)
     print('{0:7d} | {1: 15.10f} | {2:^ 15.3e} | {3:^ 15.3e} |'
-          .format(n, approx, erreur, )
+          .format(n, approx, erreur, erreur_relative),
           file=sys.stdout, flush=True)
 print('-'*63) 
